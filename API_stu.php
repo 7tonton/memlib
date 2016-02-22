e7cea38e5ac38829fae7b212afa93e38ace8e2bd
Abdurrhman <abdzz@windowslive.com>
2016-02-22 15:24:28 +0300

    write connect to database function

--- API_stu.php
+++ API_stu.php
@@ -1,9 +1,18 @@
 <?php 
 
 // Student API ::
-function memlib_stu_add($stu_id, $stu_nid, $stu_cam, $stu_fname, $stu_mname, $stu_lname, $stu_dep, $stu_email, $stu_address, $stu_mob) {
-	sprintf("INSERT INTO `stu_info`(`stu_id`, `stu_nid`, `stu_cam`, `stu_fname`, `stu_mname`, `stu_lname`, `stu_dep`, `stu_email`, `stu_address`, `stu_mob`, `stu_date`) VALUES ([%s],[%s],[%s],[%s],[%s],[%s],[%s],[%s],[%s],[%s])",
-	$stu_id, $stu_nid, $stu_cam, $stu_fname, $stu_mname, $stu_lname, $stu_dep, $stu_email, $stu_address, $stu_mob);
+
+function memlib_stu_get($extra = '') {
+	
+	$mysql_query = sprintf("SELECT * FROM `stu_info` WHERE %s = ", )
+	
 }
 
+function memlib_stu_add($stu_id, $stu_nid, $stu_cam, $stu_fname, $stu_mname, $stu_lname, $stu_dep, $stu_email, $stu_address, $stu_mob) {
+	
+	$mysql_query = sprintf("INSERT INTO `stu_info`(`stu_id`, `stu_nid`, `stu_cam`, `stu_fname`, `stu_mname`, `stu_lname`, `stu_dep`, `stu_email`, `stu_address`, `stu_mob`, `stu_date`) VALUES ([%s],[%s],[%s],[%s],[%s],[%s],[%s],[%s],[%s],[%s],[%s])",
+	$stu_id, $stu_nid, $stu_cam, $stu_fname, $stu_mname, $stu_lname, $stu_dep, $stu_email, $stu_address, $stu_mob, NULL);
+	
+}
+	
 ?>
