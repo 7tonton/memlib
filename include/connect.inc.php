51a52d87419941bf9996a494c04d557c1226672d
devno1 <dev.tonton.32@gmail.com>
2016-03-28 20:53:03 +0300

    change project schema

---
+++ include/connect.inc.php
@@ -0,0 +1,35 @@
+<?php 
+
+$mlf_host = 'localhost';
+$mlf_user = 'user1';
+$mlf_pass = 'user1';
+
+$mlf_db_name = 'libform';
+
+@$mlf_handle = mysql_connect($mlf_host, $mlf_user, $mlf_pass);
+if(!$mlf_handle) {
+	// exit('Connection problem ...');
+	echo 'Connection problem ...';
+}
+
+$mlf_db_result = mysql_select_db($mlf_db_name);
+if(!@mlf_db_result) {
+	mysql_close($mlf_handle);
+	// exit('Selection problem ...');
+	echo 'Selection problem ...';
+} 
+
+@mysql_query("SET NAMES 'utf8'"); // set the inputs to be utf8 to accept all kind of letters
+
+/*
+tinyf_db_close() the use of this name because is categorize kind of.
+				 tinyf: project name	
+				 db:    categorize
+				 close: operation
+*/
+function mlf_db_close() {
+	global $mlf_handle;
+	@mysql_close($mlf_handle);
+}
+
+?>
