43e4670a7a36e844734c768e14f6866d18318b7b
devno1 <dev.tonton.32@gmail.com>
2016-03-28 23:18:36 +0300

    create language, and configuration files

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <!DOCTYPE html>
-
 <html>
-
 <head>
-
 <meta charset="UTF-8">
 
 <title>Show Dircatinos</title>
@@ -11,44 +8,32 @@
 <style type="text/css">
 
 	.container {
-	
 		width: 800px;
-	
-		margin-left: auto;
-	
+		margin-left: auto;	
 		margin-right: auto;
-	
 		text-align: center;
 }
-
 </style>
 
 </head>
-
 <body>
 
+	<div class="container">
+		
+		<this style="font-weight: 600;">The Royal Commission of Yanbu Colleges & Institutes Libraries & Learing Resources</this> 
 
-<div class="container">
-	
-<this style="font-weight: 600;">The Royal Commission of Yanbu Colleges & Institutes Libraries & Learing Resources</this> 
-<br>
-	
-<u>MEMBERSHIP FORM</u>
-	
-<br>
-	
-<br>
-	
-<br>
-	
-<a href="visit_signup.html"> Signup For New Memebership </a>
-	
-<br>
-	
-<a href="login.php"> Confirm Memebership </a>
-</div>
+		<br>
 
+		<u>MEMBERSHIP FORM</u>
+
+		<br><br><br>
+
+		<a href="visit_signup.html"> Signup For New Memebership </a>
+
+		<br>
+
+		<a href="login.php"> Confirm Memebership </a>
+	</div>
 
 </body>
-
 </html>
